66aca6b80999e9c70f524c3305e02e81dd55771a
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-25 14:12:03 +0500

    routing through folder structure

---
+++ music/src/app/sgomez/page.tsx
@@ -0,0 +1,5 @@
+export default function SGPage() {
+  return (
+      <h1 className="text-4xl text-center mt-8">Selena Gomez</h1>
+  );
+}
---
+++ music/src/app/sgomez/usa/page.tsx
@@ -0,0 +1,5 @@
+export default function USAPage() {
+  return (
+    <h1 className="text-4xl text-center mt-8">USA</h1>
+  );
+}

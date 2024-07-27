208d0787e0b65cdc600129d5dbc67e16810e447e
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-28 03:10:47 +0500

    navbar setup with aceternity ui

--- music/src/app/page.tsx
+++ music/src/app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-4xl text-center mt-8">Music</h1>
+
+    </main>
   );
 }

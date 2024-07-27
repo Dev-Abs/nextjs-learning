9a09e66a37281277ccd007875b1e4659103435c3
Dev-Abs <abdullah.devorbit@gmail.com>
2024-07-28 02:02:58 +0500

    unnecessary files deleted

--- music/src/app/sgomez/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function SGPage() {
-  return (
-      <h1 className="text-4xl text-center mt-8">Selena Gomez</h1>
-  );
-}
--- music/src/app/sgomez/usa/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function USAPage() {
-  return (
-    <h1 className="text-4xl text-center mt-8">USA</h1>
-  );
-}

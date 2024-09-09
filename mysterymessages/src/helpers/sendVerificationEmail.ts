1aa33356f1241c95b32a3cc097cff347ac5acc53
Dev-Abs <abdullah.devorbit@gmail.com>
2024-09-10 01:01:20 +0500

    email send error handled, in test mode email can only be sent to own account

--- mysterymessages/src/helpers/sendVerificationEmail.ts
+++ mysterymessages/src/helpers/sendVerificationEmail.ts
@@ -1,6 +1,8 @@
-import { resend } from "@/lib/resend";
+// import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerficationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(
   email: string,
@@ -9,7 +11,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-        from: "onboarding@resend.dev",
+        from: "Acme <onboarding@resend.dev>",
         to: email,
         subject: "Mystery Message | Verify your email",
         react: VerificationEmail({ username, otp: verifyCode }),
